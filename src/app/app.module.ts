de59ac87897d80849d025d0002a669a0390945cd
Sara Caballero C <aracelli.caballero@gmail.com>
2017-08-21 18:09:06 -0600

    Avance con formulario de ingreso. Todavía no está listo.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { FormularioLoginComponent } from './formulario-login/formulario-login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormularioLoginComponent
   ],
   imports: [
     BrowserModule,
